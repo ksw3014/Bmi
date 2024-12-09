d0d75ace6127f8646e5662776d41cf05bf049c36
Ian <pijh.s70210@gmail.com>
2024-12-09 16:06:43 +0800

    layout, find views, onclick, multi-language

--- app/src/main/java/com/ian/bmi/MainActivity.kt
+++ app/src/main/java/com/ian/bmi/MainActivity.kt
@@ -3,7 +3,9 @@ package com.ian.bmi
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -22,11 +24,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.bHelp.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("BMI說明")
+                .setMessage("體重(kg)/身高的平方(m)")
+                .setPositiveButton("OK", null)
+                .show()
+        }
     }
-    fun bmi(view: View){
+
+    fun bmi(view: View) {
         val weight = binding.edWeight.text.toString().toFloat()
         val height = binding.edHeight.text.toString().toFloat()
         val bmi = weight / (height * height)
         Log.d("BMI", bmi.toString())
+        Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setMessage(bmi.toString())
+            .setTitle("Your BMI")
+            .setPositiveButton("OK", null)
+            .setNeutralButton("cancel", null)
+            .show()
     }
 }
